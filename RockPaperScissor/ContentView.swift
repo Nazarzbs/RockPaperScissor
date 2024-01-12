77d2dc03f5732bbee0d281edcc2c3aaa8b7a539f
Nazarzbs <famina1998@gmail.com>
2024-01-12 22:45:04 +0200

    Initial Commit

---
+++ RockPaperScissor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissor
+//
+//  Created by Nazar on 12.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Nazar on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
